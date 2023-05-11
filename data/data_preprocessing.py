66cac3f6d4b8610c8d567e0042a27ef8be7db194
oldentomato <ender1412@naver.com>
2023-05-11 22:12:18 +0900

    데이터 정제코드 수정, train코드 추가

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -1,40 +1,62 @@
-from data_type import category,code
 import os 
 import xmltodict
 from bisect import bisect_left
 from sys import stdout
 import shutil
+from use_list import food_list
+
+
+def Search_Img(find_file) -> int:
+    img_path = "D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\images"
+    img_path2 = "D:\\yaming_dataset\\dataset\\temp\\images"
+    file_list = os.listdir(img_path+"/")
+    file_list2 = os.listdir(img_path2+"/")
+    if (find_file+".jpg" in file_list) or (find_file+".jpeg" in file_list) or (find_file+".JPG" in file_list) or (find_file+".JPEG" in file_list):
+        return 0
+    elif (find_file+".jpg" in file_list2) or (find_file+".jpeg" in file_list2) or (find_file+".JPG" in file_list2) or (find_file+".JPEG" in file_list2):
+        return 1
+    else:
+        return 2
+
+
+def Test():
+    # print(Search_Img("01_012_01012003_160553522146917_0"))
+    shutil.copy("D:\\yaming_dataset\\dataset\\temp\\images/01_012_01012003_160553522146917_0.jpg", "D:\\yaming_dataset\\dataset_2\\image/01_012_01012003_160553522146917_0.jpg")
+
 
 
 def Parsing_XML():
-    xml_path = 'D:\\yaming_dataset\\[라벨]음식분류_TRAIN\\xml'
-    txt_path = 'D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\labels'
+    xml_path = 'D:\\yaming_dataset\\[라벨]음식분류_TRAIN\\xml/'
+    img_path = 'D:\\yaming_dataset\\음식 이미지 및 영양정보 텍스트\\Training/'
+    
+    txt_dir_path = 'D:\\yaming_dataset\\dataset_2\\label'
+    img_dir_path = 'D:\\yaming_dataset\\dataset_2\\image'
+
     dir_list = os.listdir(xml_path+"/")
     data_max_count = 499
-    passed_arr = []
     
     for dir in dir_list:
         file_list = os.listdir(xml_path+"/"+dir+"/")
         for index,file in enumerate(file_list):
-            if index > data_max_count:
+            #특정 음식코드이거나 500개가 넘어간다면
+            if (index > data_max_count) or (dir in food_list):
                 break
             target = xmltodict.parse(open(xml_path+"/"+dir+"/"+file ,encoding="UTF-8").read())
 
             #몇몇 파일에 이름이 깨지는 경우가 있어서 파일명으로 카테고리 지정으로 변경
-            class_index = bisect_left(code,file.split("_")[2])
+            # class의 index 휙득(이진탐색으로 검색)
+            class_index = bisect_left(food_list,file.split("_")[2])
             #간혼 size 가 없는 xml 이 있을경우 예외처리
             try:
                 Image_Height = float(target['annotation']['size']['height'])
                 Image_Width  = float(target['annotation']['size']['width'])
             except:
-                passed_arr.append(file)
+                # passed_arr.append(file)
                 continue
-            with(open(txt_path + "/" + file[:-4]+".txt",mode="w")) as label_file:
+            with(open(txt_dir_path + "/" + file[:-4]+".txt",mode="w")) as label_file:
                 for obj in target['annotation']['object']:
-                    
-                    # class의 index 휙득(이진탐색으로 검색)
+                    #접시 정보는 패스
                     if obj['name'] == "dish":
-                        # class_index = 401
                         continue
                         
                     # min, max좌표 얻기
@@ -55,117 +77,32 @@ def Parsing_XML():
                     label_file.write(
                         f"{class_index} {x} {y} {w} {h}\n"
                     )
+                #이미지 이동(jpg or jpeg)
+                # 이미지를 이미 옮긴 이력이 있어서 파일 검색 후 처리
+                search_result = Search_Img(file[:-4])
+
+                if search_result == 0:
+                    try:
+                        shutil.copy("D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\images/"+file[:-4]+".jpg", img_dir_path+"/"+file[:-4]+".jpg")
+                    except:
+                        shutil.copy("D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\images/"+file[:-4]+".jpeg", img_dir_path+"/"+file[:-4]+".jpeg")
+                elif search_result == 1:
+                    try:
+                        shutil.copy("D:\\yaming_dataset\\dataset\\temp\\images/"+file[:-4]+".jpg", img_dir_path+"/"+file[:-4]+".jpg")
+                    except:
+                        shutil.copy("D:\\yaming_dataset\\dataset\\temp\\images/"+file[:-4]+".jpeg", img_dir_path+"/"+file[:-4]+".jpeg")
+                else:
+                    try:
+                        shutil.copy(img_path+dir+"/"+file[:-4]+".jpg", img_dir_path+"/"+file[:-4]+".jpg")
+                    except:
+                        shutil.copy(img_path+dir+"/"+file[:-4]+".jpeg", img_dir_path+"/"+file[:-4]+".jpeg")
+
 
                 stdout.write("\r===== In  "+dir+"  DIR : "+str(index+1)+" progressed=====")
                 stdout.flush()
 
-    with(open("./Passed_arr.txt",mode="w")) as file:
-        for i in passed_arr:
-            file.write(
-                f"{i}\n"
-            )
-
-#카테고리별로 500개씩 파일을 옮김
-def ImageFileMove():
-    img_path = "D:\\yaming_dataset\\음식 이미지 및 영양정보 텍스트\\Training/"
-    dir_path = "D:\\yaming_dataset\\dataset\\train\\images/"
-    file_list = os.listdir(img_path)
-    pass_count = 0
-
-
-    for file in file_list:
-        imgs = os.listdir(img_path+file+"/")
-        for count,img in enumerate(imgs):
-            if count > 499:
-                print("total: "+str(pass_count)+"passed")
-                break
-            
-            shutil.move(img_path+file+"/"+img, dir_path)
-            stdout.write("\r===== "+ str(count+1) +" : "+str(500)+" progressed=====")
-            stdout.flush()
-
-
-
-def RemovePassImage():
-    pass_arr = []
-    img_path = "D:\\yaming_dataset\\dataset\\train\\images/"
-    file_list = os.listdir(img_path)
-    with(open("./Passed_arr.txt",mode="r")) as file:#통과된 파일리스트 txt 불러오고 배열에 담기
-        pass_arr = list(map(lambda s: s.strip(), file))
-
-    for file in pass_arr:
-        result = file in pass_arr #.jpg과 숫자 두개 더 지우고 .xml로 바꾼 뒤 검색
-        if result == True:
-            #운좋게 한번에 잘 실행됬지만 remove는 휴지통이동이 아니라 아예 삭제이기때문에
-            #다음에는 파일이동으로 바꿀것
-            try:
-                os.remove(img_path+file[:-4]+".jpg")
-            except:
-                os.remove(img_path+file[:-4]+".jpeg")
-
-#txt 파일내 카테고리번호가 이상치가 있는 것 같아서 파일들을 순회하면 카테고리코드 확인
-def Debuging():
-    txt_path = 'D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\labels/'
-    file_list = os.listdir(txt_path)
-    for count,file in enumerate(file_list):
-        with(open(txt_path+file,mode="r")) as text:
-            line = text.readline()
-            line = line.split()
-            if (int(line[0]) > 399) or (int(line[0]) < 0):
-                print("trouble detected in "+file+" value: "+line[0])
-        stdout.write("\r===== "+ str(count+1) +" : "+str(len(file_list))+" progressed=====")
-        stdout.flush()
-    
-#이상치가 확인이되어 xml파일내에 정보가 잘못되었는지 확인해봄   
-#결과로 이름이 깨져있어서 xml파일명에 있는 음식코드를 통해 인덱스조정으로 수정함
-def test():
-    xml_path = 'D:\\yaming_dataset\\[라벨]음식분류_TRAIN\\xml/01014007/01_014_01014007_160311628224367_0.xml'
-
-    target = xmltodict.parse(open(xml_path ,encoding="UTF-8").read())
-    print(target['annotation']['object'][1]['name'])
-
-#500개의 파일이 너무많아 카테고리별로 100개씩 줄이는 코드 (20만개에서 4만개로 줄임)
-def Data_Reduction():
-    txt_path = 'D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\labels/'
-    img_path = 'D:\\yaming_dataset\\Yaming_AI\\yolov7\\dataset\\images/'
-
-    temp_txt_path = "D:\\yaming_dataset\\dataset\\temp\\labels/"
-    temp_img_path = "D:\\yaming_dataset\\dataset\\temp\\images/"
-
-    count = 0
-
-    txt_list = os.listdir(temp_txt_path)
-    img_list = os.listdir(temp_img_path)
-
-    for code_str in code:
-        for file in txt_list:
-            if count > 99:
-                break
-            if file.split("_")[2] == code_str:
-                shutil.move(temp_txt_path+file, txt_path)
-                count += 1
-                stdout.write("\r===== "+code_str+" : "+str(count)+" progressed=====")
-                stdout.flush()
-
-        count = 0
-
-    print("text_file : done")
-    for code_str in code:
-        for file in img_list:
-            if count > 99:
-                break
-            if file.split("_")[2] == code_str:
-                shutil.move(temp_img_path+file, img_path)
-                count += 1
-                stdout.write("\r===== "+code_str+" : "+str(count)+" progressed=====")
-                stdout.flush()
-
-        count = 0
-
 
 if __name__ == "__main__":
-    # Parsing_XML()
-    # ImageFileMove()
-    # RemovePassImage()
-    # Debuging()
-    Data_Reduction()
+    Parsing_XML()
+    # Test()
+
